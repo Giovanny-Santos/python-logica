fce643289d0cf153bfef97217abac53fb00ac013
Giovanny Santos <geovanny_555@outlook.com>
2026-03-08 12:16:32 -0300

    exercicio

---
+++ exercicio035.py
@@ -0,0 +1,10 @@
+print("Digite o seu turno: ")
+turno = input()
+if turno == "manhã":
+    print("Bom dia!")
+elif turno == "tarde":
+    print("Boa tarde!")
+elif turno == "noite":
+    print("Boa noite!")
+else:
+    print("Turno inválido.")
---
+++ exercicio036.py
@@ -0,0 +1,7 @@
+print("digite o valor: ")
+valor = input()
+valor = int(valor)
+if valor % 2 == 0:
+    print("O valor é par.")
+else:
+    print("O valor é ímpar.")
